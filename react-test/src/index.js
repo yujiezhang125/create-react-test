678977ac2506630c741761673ab52e445b1e0c06
Yujie Zhang <1369616003@qq.com>
2022-04-06 22:22:56 -0700

    basic version

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import route
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './pages/Home';
+import LogInPage from './pages/LogIn';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render((
+  <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='login' element={<LogInPage />} />
+      </Routes>
+    </BrowserRouter>
+), document.getElementById('root'));
